31052edc3acc86d1f2302d5a661ade35d421236a
samx03 <sameerbakshi34@gmail.com>
2021-08-24 17:24:44 +0530

    Back to top Btn

--- js/index.js
+++ js/index.js
@@ -17,3 +17,24 @@ toggleBtn.addEventListener("click", function(){
         linksContainer.style.height = 0;
     }
 });
+
+
+const navContainer = document.querySelector(".navbar");
+const topBtn = document.querySelector(".top-btn"); 
+window.addEventListener("scroll", function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navContainer.getBoundingClientRect().height;
+    
+    if( scrollHeight > navHeight){
+        navContainer.classList.add("fixed-nav");
+    }
+    else{
+        navContainer.classList.remove("fixed-nav");
+    }
+    if(scrollHeight > 500){
+        topBtn.classList.add("show-btn");
+    }
+    else{
+        topBtn.classList.remove("show-btn");
+    }
+});
